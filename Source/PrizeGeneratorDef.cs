f1bf6cfce9043a54d4ae251155495b024e825e40
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-04-01 21:15:02 +0200

    created PrizeGeneratorDef

---
+++ Source/PrizeGeneratorDef.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Verse;
+
+namespace ArchoGacha;
+
+public class PrizeGeneratorDef : Def
+{
+    public Type prizeWorkerClass = typeof(PrizeWorker);
+
+    [Unsaved] private PrizeWorker workerInt;
+
+    public float minJackpotMarketValue;
+    public float minConsolationMarketValue;
+    public PrizeWorker Worker
+    {
+        get
+        {
+            if (workerInt != null) return workerInt;
+
+            workerInt = (PrizeWorker)Activator.CreateInstance(prizeWorkerClass);
+            workerInt.def = this;
+            return workerInt;
+        }
+    }
+}
---
+++ Source/PrizeWorker.cs
@@ -0,0 +1,26 @@
+using Verse;
+
+namespace ArchoGacha;
+
+public abstract class PrizeWorker
+{
+    public PrizeGeneratorDef def;
+    public abstract Thing GeneratePrize(PrizeCategory prizeCategory);
+
+    public bool PrizeMarketValue(float baseMarketValue, PrizeCategory prizeCategory)
+    {
+        if (prizeCategory == PrizeCategory.Jackpot) return baseMarketValue * 2.5f + 1500 >= def.minJackpotMarketValue;
+
+        return baseMarketValue * 1.5f + 500 >= def.minConsolationMarketValue;
+    }
+}
+
+public enum PrizeCategory
+{
+    Jackpot,
+    Consolation
+}
+
+public static class PrizeWorkerUtils
+{
+}

b58deeee7ebcd76b25955b737f4ecd5ffa88a8da
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-04-01 21:16:05 +0200

    added WeaponPrize PrizeGeneratorDef and QualityWeaponPrizeWorker

---
+++ Source/PrizeWorkers/QualityWeaponPrizeWorker.cs
@@ -0,0 +1,38 @@
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace ArchoGacha.PrizeWorkers;
+
+public class QualityWeaponPrizeWorker : PrizeWorker
+{
+    public override Thing GeneratePrize(PrizeCategory prizeCategory)
+    {
+        var prizeDef = DefDatabase<ThingDef>.AllDefsListForReading
+            .Where(t => t.thingCategories.Contains(ThingCategoryDefOf.Weapons) &&
+                        (
+                            (t.techLevel >= TechLevel.Spacer && t.HasComp<CompQuality>()) ||
+                            (t.MadeFromStuff && GenStuff.AllowedStuffsFor(t).Any(stuff =>
+                                PrizeMarketValue(StatWorker_MarketValue.CalculatedBaseMarketValue(t, stuff),
+                                    prizeCategory)))
+                        )).RandomElement();
+
+        ThingDef stuffDef = null;
+        if (prizeDef.MadeFromStuff)
+        {
+            stuffDef = GenStuff.AllowedStuffsFor(prizeDef).Where(stuff =>
+                    PrizeMarketValue(StatWorker_MarketValue.CalculatedBaseMarketValue(prizeDef, stuff), prizeCategory))
+                .RandomElement();
+        }
+
+        var prize = ThingMaker.MakeThing(prizeDef, stuffDef);
+
+        prize.TryGetComp<CompQuality>()?.SetQuality(
+            QualityUtility.GenerateQuality(prizeCategory == PrizeCategory.Jackpot
+                ? QualityGenerator.Super
+                : QualityGenerator.Reward),
+            ArtGenerationContext.Colony);
+
+        return prize;
+    }
+}

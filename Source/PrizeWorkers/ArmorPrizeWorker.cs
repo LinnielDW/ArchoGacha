f6cca8572f27d580dc2bca0affcdfec5d6be1fcb
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-04-04 16:36:04 +0200

    implemented ArchoGacha_Armor and ArmorPrizeWorker

---
+++ Source/PrizeWorkers/ArmorPrizeWorker.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace ArchoGacha.PrizeWorkers;
+
+public class ArmorPrizeWorker : PrizeWorker
+{
+    public override Thing GeneratePrize(PrizeCategory prizeCategory)
+    {
+        var stuffPair = SelectPrizeDef(prizeCategory);
+        var prize = ThingMaker.MakeThing(stuffPair.thing, stuffPair.stuff);
+
+        prize.TryGetComp<CompQuality>()?.SetQuality(
+            QualityUtility.GenerateQuality(prizeCategory == PrizeCategory.Jackpot
+                ? QualityGenerator.Super
+                : QualityGenerator.Reward),
+            ArtGenerationContext.Colony);
+
+        return prize;
+    }
+
+    public override ThingStuffPair SelectPrizeDef(PrizeCategory prizeCategory)
+    {
+        return PawnApparelGenerator.allApparelPairs.Where(stuffPair =>
+            PrizeMarketValue(stuffPair, prizeCategory) && stuffPair.thing.techLevel >= TechLevel.Industrial &&
+            ThingSetMakerUtility.CanGenerate(stuffPair.thing)).RandomElement();
+    }
+}
